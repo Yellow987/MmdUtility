a78ca023b67c5c749f0580e5a5bb61c621c83bb0
ousttrue <ousttrue@gmail.com>
2023-07-05 10:40:33 +0900

    latest blender

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@ bl_info={
     "name": "MmdUtil(pmd/pmx)",
     "author": "ousttrue",
     "version": (0, 1, 0),
-    "blender": (2, 78, 0),
+    "blender": (3, 0, 0),
     "location": "File > Import-Export",
     "description": "Import-Export PMD/PMX meshes",
     "warning": "",
-    "support": "TESTING",
+    # "support": "TESTING",
     "wiki_url": "https://github.com/ousttrue/MmdUtility",
     "tracker_url": "https://github.com/ousttrue/MmdUtility",
 }
@@ -17,26 +17,26 @@ if "bpy" in locals():
     import imp
     imp.reload(import_pmx)
     imp.reload(export_pmx)
-else:
-    from . import import_pmx
-    from . import export_pmx
+
 import bpy
+from . import import_pmx
+from . import export_pmx
 
 
 def register():
-    if not bpy.context.user_preferences.system.use_international_fonts:
-        print("enable use_international_fonts")
-        bpy.context.user_preferences.system.use_international_fonts = True
+    # if not bpy.context.user_preferences.system.use_international_fonts:
+    #     print("enable use_international_fonts")
+    #     bpy.context.user_preferences.system.use_international_fonts = True
 
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_file_import.append(import_pmx.ImportPmx.menu_func)
-    bpy.types.INFO_MT_file_export.append(export_pmx.ExportPmx.menu_func)
+    bpy.utils.register_class(import_pmx.ImportPmx)
+    bpy.types.TOPBAR_MT_file_import.append(import_pmx.menu_func)
+    # bpy.types.INFO_MT_file_export.append(export_pmx.ExportPmx.menu_func)
 
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
-    bpy.types.INFO_MT_file_import.remove(import_pmx.ImportPmx.menu_func)
-    bpy.types.INFO_MT_file_export.remove(export_pmx.ExportPmx.menu_func)
+    bpy.utils.unregister_class(import_pmx.ImportPmx)
+    # bpy.types.INFO_MT_file_import.remove(import_pmx.ImportPmx.menu_func)
+    # bpy.types.INFO_MT_file_export.remove(export_pmx.ExportPmx.menu_func)
 
 
 if __name__ == "__main__":
